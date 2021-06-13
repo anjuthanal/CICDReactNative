ed6ad85ea8a1df686feafd2ea0d5e120bc9a4df9
anju <anju.thanal@gmail.com>
2021-06-14 00:55:41 +0530

    Display entered name functionality addded

--- Screens/InputComponent.js
+++ Screens/InputComponent.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
 import { View,Text,TextInput,StyleSheet, Button } from "react-native";
 import Crashes from "appcenter-crashes";
 
 const InputComponent = (props) => {
+  const[banner,setBanner] = useState('');
+  const setNameHandler = (enteredName) =>{
+    setBanner(enteredName);
+  }
+
+  const[nameList,setNameList] = useState([]);
+  const getNameList = () =>{
+    setNameList([...nameList,banner])
+    setBanner('');
+  }
   return(
     <View style={styles.container}>
       <TextInput  
         style={styles.textInputContainer} 
-        placeholder="Enter Name"/>
-      <Button title='ADD' onPress={()=> Crashes.generateTestCrash()}/>
+        placeholder="Enter Name"
+        onChangeText={setNameHandler}
+        value={banner}/>
+      <Button title='ADD' 
+      onPress={getNameList}/>
+      <Text>Hello : {nameList.join('*')}</Text>
     </View>
   );
 }
